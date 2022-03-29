4e3fcebe135a93279eb990d3d93a75577d45f231
haise0807 <94881080+haise0807@users.noreply.github.com>
2022-03-29 16:00:00 +0900

    itemsテーブルの編集

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2022_03_29_053417) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.text "information", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false

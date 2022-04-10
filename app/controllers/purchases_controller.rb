fd8c800c9189c5bebdf0416601ff5b8c816d5aee
haise0807 <94881080+haise0807@users.noreply.github.com>
2022-04-10 16:48:02 +0900

    purchasesコントローラー作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,23 @@
 class PurchasesController < ApplicationController
   def index
-    
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    binding.pry
+    if @purchase_address.valid?
+      @purchase_address.save
+    redirect_to root_path
+   else
+     render :index
+   end
+  end
+
+  private
+
+  def purchase_params
+   params.require(:purchase_address).permit( :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end

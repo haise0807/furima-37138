c8175c77c5fef233fa904fe48f6b9a757a48de7e
haise0807 <94881080+haise0807@users.noreply.github.com>
2022-04-17 06:33:30 +0900

    PurchaseAddressのバリデーション

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,11 +6,10 @@ class PurchaseAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code
+    validates :post_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :house_number
-    validates :phone_number
-    validates :purchase_id
+    validates :phone_number,  format:{with: /\A\d{10,11}\z/}
   end
   validates :prefecture_id,   numericality: { other_than: 1, message: "can't be blank" }
 
@@ -18,4 +17,6 @@ class PurchaseAddress
     purchase = Purchase.create(user_id: user_id, item_id: item_id )
     Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number,purchase_id: purchase.id)
   end
+
+  
 end

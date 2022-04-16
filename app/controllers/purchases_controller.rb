250c3b10f639e551010d8702a89e5e9d6894b776
haise0807 <94881080+haise0807@users.noreply.github.com>
2022-04-17 06:32:06 +0900

    購入画面表示・購入ボタン後の遷移

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,12 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
     @purchase_address = PurchaseAddress.new
   end
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
-    binding.pry
     if @purchase_address.valid?
       @purchase_address.save
     redirect_to root_path
@@ -17,7 +18,10 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-   params.require(:purchase_address).permit( :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_id).merge(user_id: current_user.id, item_id: params[:item_id])
+   params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
